ea15e25fdd238ea61c9d40b69ea4777d5ac5fed2
Mariam <samysakr929@gmail.com>
2025-10-27 03:09:50 +0300

    add all function

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,26 @@
-void main() {
-  
+void main() {}
+
+// Basic Function
+void Function() {
+  print('Hello');
 }
+
+// Function with Parameters
+void Function1(int a,String b) {
+  print('a: $a, b: $b');
+}
+
+// Function with Return Type
+int Function2(int x, int y) {
+  return x + y;
+}
+
+// Named Parameters
+void Function3({required String name, int age = 0,int? n2}) {
+  print('Name: $name, Age: $age');
+}
+// ? => Nullable Parameter
+// {} => Named Parameters
+// [] => Optional Positional Parameters
+// required => Mandatory Named Parameters
+// Named function is a Optional Parameter
